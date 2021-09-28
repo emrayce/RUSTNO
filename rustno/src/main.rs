6a967c83897d83d38c7c86fb4cad11ff33c3e40e
cedric <cedric.parpet@orange.fr>
2021-09-28 15:53:44 +0200

    Create players and a deck

--- rustno/src/main.rs
+++ rustno/src/main.rs
@@ -1,3 +1,22 @@
+mod card;
+use card::{
+    Card, Symbol, Color
+};
+
 fn main() {
-    println!("Hello, world!");
+    // Create players
+    let players = vec![0, 0, 0, 0];
+
+    //Create deck and playfield
+    let mut deck = Vec::new();
+    for i in 1..5 {
+        deck.push(Card::new(Color::RED, Symbol::ONE));
+    }
+
+    for i in deck.iter() {
+        println!("{:?}", i);
+    }
+    // Distribute cards to player
+    // Create order and first to play
+    // Gameplay loop;
 }
