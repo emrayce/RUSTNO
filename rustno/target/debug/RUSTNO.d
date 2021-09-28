c00fa46b8161345e345621d4a7c08d432d830772
cedric <cedric.parpet@orange.fr>
2021-09-28 14:25:43 +0200

    Add a cargo project rustno

---
+++ rustno/target/debug/RUSTNO.d
@@ -0,0 +1,1 @@
+/home/emrayce/Documents/RUSTNO/rustno/target/debug/RUSTNO: /home/emrayce/Documents/RUSTNO/rustno/src/main.rs
---
+++ rustno/target/debug/deps/RUSTNO-d21da3d535b92e12.d
@@ -0,0 +1,5 @@
+/home/emrayce/Documents/RUSTNO/rustno/target/debug/deps/RUSTNO-d21da3d535b92e12: src/main.rs
+
+/home/emrayce/Documents/RUSTNO/rustno/target/debug/deps/RUSTNO-d21da3d535b92e12.d: src/main.rs
+
+src/main.rs:
