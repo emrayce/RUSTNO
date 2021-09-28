ba42cd05077428c5ad6328da3c358428b6409312
cedric <cedric.parpet@orange.fr>
2021-09-28 14:26:13 +0200

    Add a new struct Card

---
+++ rustno/src/card.rs
@@ -0,0 +1,29 @@
+enum Symbol {
+    ONE,
+    TWO,
+    THREE,
+    FOUR,
+    FIVE,
+    SIX,
+    SEVEN,
+    EIGHT,
+    NINE,
+    STOP,
+    PLUS_2,
+    PLUS_4,
+    CHANGE_COLOR,
+    CANGE_DIRECTION,
+}
+
+enum Color {
+    RED,
+    YELLOW,
+    BLUE,
+    GREEN,
+}
+
+struct Card {
+    // Symbol
+    // Color
+    // Effect
+}
